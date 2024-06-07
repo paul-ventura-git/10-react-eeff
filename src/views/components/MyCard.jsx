9c26e9357efd2721a11938ff767e7773cc84a4ba
Paul Ventura <j.paul.ventura@gmail.com>
2024-06-07 07:35:49 -0500

    Card

--- src/views/components/MyCard.jsx
+++ src/views/components/MyCard.jsx
@@ -11,67 +11,115 @@ function MyCard( props ) {
       <div className="card-body">
         <h5 className="card-title">Registros del libro diario</h5>
         <p className="card-text">Operaciones del día: 06/06/2024</p>
-        <Row>
-          <div className="col-sm-8">
-            <div className="card">
-              <div className="card-body">
-                <h5 className="card-title">--------------x------------</h5>
-                <p className="card-text">65 OTROS GASTOS DE GESTIÓN</p>
-                <p className="card-subtext"> 655 Costo neto de enajenación de activos inmovilizados y operaciones discontinuadas</p>
-              </div>
+        <div className='entry py-2'>
+          <table className="table table-sm">
+            <thead className='table-light'>
+              <tr>
+                <th scope="col">#</th>
+                <th scope="col">-----------------------x-------------------</th>
+                <th scope="col">Debe</th>
+                <th scope="col">Haber</th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr>
+                <th scope="row">43</th>
+                <td >
+                  <p className='text-start fw-bold'>CUENTAS POR PAGAR COMERCIALES RELACIONADAS</p>
+                  <p className="card-subtext text-start"> 432 Anticipos otorgados</p>
+                  <p className="card-subtext text-start"> 4321 Anticipos otorgados</p>
+                </td>
+                <td>50,000</td>
+                <td>-</td>
+              </tr>
+              <tr>
+                <th scope="row">40</th>
+                <td >
+                  <p className='text-start fw-bold'>TRIBUTOS, CONTRAPRESTACIONES Y APORTES AL SISTEMA PÚBLICO DE PENSIONES Y DE SALUD POR PAGAR</p>
+                  <p className="card-subtext text-start"> 401 Gobierno nacional</p>
+                  <p className="card-subtext text-start"> 4011 Impuesto general a las ventas</p>
+                  <p className="card-subtext text-start"> 40116 IGV destinado a operaciones gravadas</p>
+                </td>
+                <td>9,000</td>
+                <td>-</td>
+              </tr>
+              <tr>
+                <th scope="row">43</th>
+                <td >
+                  <p className='text-start fw-bold'>CUENTAS POR PAGAR COMERCIALES RELACIONADAS</p>
+                  <p className="card-subtext text-start"> 431 Facturas, boletas y otros comprobantes por pagar</p>
+                  <p className="card-subtext text-start"> 4312 Emitidas</p>
+                </td>
+                <td>-</td>
+                <td>59,000</td>
+              </tr>
+            </tbody> 
+          </table>
+          <Row className="row justify-content-start mt-4">
+            <div className="col-1">
+              <Link to="/" className="btn btn-primary">Agregar</Link>
             </div>
-          </div>
-          <div className="col-sm-2">
-            <div className="card">
-              <div className="card-body">
-                <h5 className="card-title">Debe</h5>
-                <p className="card-text">80000</p>
-              </div>
+            <div className="col-1">
+              <Link to="/" className="btn btn-primary">Eliminar</Link>
             </div>
-          </div>
-          <div className="col-sm-2">
-            <div className="card">
-              <div className="card-body">
-                <h5 className="card-title">Haber</h5>
-                <p className="card-text">80000</p>
-              </div>
+          </Row>
+        </div>
+
+        <div className='entry py-2'>
+          <table className="table table-sm">
+            <thead className='table-light'>
+              <tr>
+                <th scope="col">#</th>
+                <th scope="col">-----------------------x-------------------</th>
+                <th scope="col">Debe</th>
+                <th scope="col">Haber</th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr>
+                <th scope="row">43</th>
+                <td >
+                  <p className='text-start fw-bold'>CUENTAS POR PAGAR COMERCIALES RELACIONADAS</p>
+                  <p className="card-subtext text-start"> 432 Anticipos otorgados</p>
+                  <p className="card-subtext text-start"> 4321 Anticipos otorgados</p>
+                </td>
+                <td>50,000</td>
+                <td>-</td>
+              </tr>
+              <tr>
+                <th scope="row">40</th>
+                <td >
+                  <p className='text-start fw-bold'>TRIBUTOS, CONTRAPRESTACIONES Y APORTES AL SISTEMA PÚBLICO DE PENSIONES Y DE SALUD POR PAGAR</p>
+                  <p className="card-subtext text-start"> 401 Gobierno nacional</p>
+                  <p className="card-subtext text-start"> 4011 Impuesto general a las ventas</p>
+                  <p className="card-subtext text-start"> 40116 IGV destinado a operaciones gravadas</p>
+                </td>
+                <td>9,000</td>
+                <td>-</td>
+              </tr>
+              <tr>
+                <th scope="row">43</th>
+                <td >
+                  <p className='text-start fw-bold'>CUENTAS POR PAGAR COMERCIALES RELACIONADAS</p>
+                  <p className="card-subtext text-start"> 431 Facturas, boletas y otros comprobantes por pagar</p>
+                  <p className="card-subtext text-start"> 4312 Emitidas</p>
+                </td>
+                <td>-</td>
+                <td>59,000</td>
+              </tr>
+            </tbody> 
+          </table>
+          <Row className="row justify-content-start mt-4">
+            <div className="col-1">
+              <Link to="/" className="btn btn-primary">Agregar</Link>
             </div>
-          </div>
-        </Row>
-        <Row>
-          <div className="col-sm-8">
-            <div className="card">
-              <div className="card-body">
-                <h5 className="card-title">--------------x------------</h5>
-                <p className="card-text">39 DEPRECIACIÓN Y AMORTIZACIÓN ACUMULADOS</p>
-              </div>
+            <div className="col-1">
+              <Link to="/" className="btn btn-primary">Eliminar</Link>
             </div>
-          </div>
-          <div className="col-sm-2">
-            <div className="card">
-              <div className="card-body">
-                <h5 className="card-title">Debe</h5>
-                <p className="card-text">80000</p>
-              </div>
-            </div>
-          </div>
-          <div className="col-sm-2">
-            <div className="card">
-              <div className="card-body">
-                <h5 className="card-title">Haber</h5>
-                <p className="card-text">80000</p>
-              </div>
-            </div>
-          </div>
-        </Row>
-        <Row className="row justify-content-start mt-4">
-          <div className="col-1">
-            <Link to="/" className="btn btn-primary">Agregar</Link>
-          </div>
-          <div className="col-1">
-            <Link to="/" className="btn btn-primary">Eliminar</Link>
-          </div>
-        </Row>        
+          </Row>
+        </div>
+
+      
       </div>
       <div className="card-footer text-muted">
         2 days ago
